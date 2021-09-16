e0594c9b1ee5a8061fbea83330b74b203f9d3ac3
agn-g <agnieszka.it@outlook.com>
2021-09-16 13:03:40 +0200

    [Day001] Added function for converting degrees

--- converter/converter.py
+++ converter/converter.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
+def convert(input_format, output_format, value):
+    if input_format == output_format:
+        return value
+    if input_format == 'C':
+        return value * 1.8 + 32
+    else:
+        return (value - 32) * 0.5556
+
+
 celsius = int(input('Enter temperature in Celsius: '))
-fahrenheit = celsius * 1.8 + 32
+fahrenheit = convert('C', 'F', celsius)
 print(f'{celsius}*C equals to {fahrenheit}*F')
 
 fahrenheit = int(input('Enter temperature in Fahrenheit: '))
-celsius = (fahrenheit - 32) * 0.5556
+celsius = convert('F', 'C', fahrenheit)
 print(f'{fahrenheit}*C equals to {celsius}*F')
