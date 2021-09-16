40eae4aff3e66d10dc3c76fbd4caf980b2c8fd6c
agn-g <agnieszka.it@outlook.com>
2021-09-16 11:39:44 +0200

    [Day001] Added additional conversion from F to C

--- converter/converter.py
+++ converter/converter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
 celsius = int(input('Enter temperature in Celsius: '))
-fahrenheit = (celsius * 1.8) + 32
-print(f'{celsius}*C equals to {fahrenheit} *F')
+fahrenheit = celsius * 1.8 + 32
+print(f'{celsius}*C equals to {fahrenheit}*F')
+
+fahrenheit = int(input('Enter temperature in Fahrenheit: '))
+celsius = (fahrenheit - 32) * 0.5556
+print(f'{fahrenheit}*C equals to {celsius}*F')
